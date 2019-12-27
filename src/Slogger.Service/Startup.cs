8524de4cc1ee0707a8b2cda2409d0e62bbb6f250
dimohy <dimohy01@gmail.com>
2019-12-28 00:16:33 +0900

    Changed project name from Slogger to Slogger.Service.

--- src/Slogger.Service/Startup.cs
+++ src/Slogger.Service/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Slogger.Data;
 
-namespace Slogger
+namespace Slogger.Service
 {
     public class Startup
     {
